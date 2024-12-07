cd565f599836229e63d98a26d412d02762b8dac6
Shambu2 <mugalishambu601@gmail.com>
2024-12-07 09:40:23 +0530

    first commit

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -4,7 +4,7 @@ const authConfig = {
 			// Here I'm getting the domain value from process.env.CLERK_ISSUER_URL. If you want to do the same thing set this as an environment variable in your Convex Dashboard
 			// This is something I updated after the video. The reason is: we would like to keep our secrets out of our codebase.
 			//
-			domain: process.env.CLERK_ISSUER_URL,
+			domain:"https://certain-herring-62.clerk.accounts.dev",
 			applicationID: "convex",
 		},
 	],
